f77a78db835725dcd8809751466de40c8d766f8a
PankajKrishnaVanshaj <pankajyaduvanshi108@gmail.com>
2025-09-25 23:50:31 +0530

    amp update

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,17 +1,9 @@
 import axios from "axios";
 
-export function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    // Client-side → relative URL works
-    return "";
-  }
-  // Server-side → must use absolute URL
-  return process.env.NEXT_PUBLIC_BASE_URL;
-}
 
 // Create an Axios instance
 export const apiClient = axios.create({
-  baseURL: getBaseUrl(),
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
